1ff4ed534471d1581d67550bb4944adace76fde0
Mingsicogi <codedoctor119@gmail.com>
2019-12-05 02:02:31 +0900

    + 영속성 컨택스트 sample code 추가

--- src/main/java/hellojpa/JPAMain.java
+++ src/main/java/hellojpa/JPAMain.java
@@ -47,6 +47,34 @@ public class JPAMain {
             System.out.println("name = " + m.getName());
         }
 
+        System.out.println("========================================================");
+        System.out.println("========================================================");
+
+
+        tx.begin();
+        em.remove(em.find(Member.class, 150L));
+        em.remove(em.find(Member.class, 160L));
+        tx.commit();
+
+
+        tx.begin();
+        Member member1 = new Member(150L, "min1");
+        Member member2 = new Member(160L, "min2");
+
+        // insert 할 쿼리를 모아서 한번에 쿼리를 수행함.
+        em.persist(member1);
+        em.persist(member2);
+
+        System.out.println("=== persist complete ===");
+        tx.commit();
+
+
+        tx.begin();
+        Member member3 = em.find(Member.class, 150L);
+        member3.setName("ZZZZZZZZZZ");
+        tx.commit();
+
+
         em.close();
         emf.close();
 
@@ -63,6 +91,15 @@ public class JPAMain {
             // 영속 상태. DB 에 저장된 상태가 아님.
             JPAMain.em.persist(member);
 //            em.detach(member); // 영속 상태 해제.
+
+            // 1차 캐쉬 상태이기 때문에 DB select를 하지 않음.
+            Member dbInfo = em.find(Member.class, 3L);
+            System.out.println("name : " + dbInfo.getName());
+            Member member1 = em.find(Member.class, 3L);
+
+            System.out.println("dbInfo == member1 : " + (dbInfo == member1));
+
+
             tx.commit(); // DB에 저장완료
         } catch (Exception e){
             tx.rollback();
